6eae2021a54123605fe74959cf7264b698342cf5
Mairaj_Minhas <mairaj.minhas@northbaysolutions.net>
2024-06-26 11:37:32 +0500

    Fixed build issues

--- ChinarDialysisCenter.Business/obj/Debug/net6.0/ChinarDialysisCenter.Business.AssemblyInfo.cs
+++ ChinarDialysisCenter.Business/obj/Debug/net6.0/ChinarDialysisCenter.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChinarDialysisCenter.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+253cfd1daefe3ff8f9f55df84177f94ad564b0d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChinarDialysisCenter.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChinarDialysisCenter.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ChinarDialysisCenter.Server/Controllers/MemberShipController.cs
+++ ChinarDialysisCenter.Server/Controllers/MemberShipController.cs
@@ -14,23 +14,25 @@ namespace ChinarDialysisCenter.Server.Controllers
         {
             this.manageMemberships = manageMemberships;
         }
+        [Route("GetMembers")]
+        [HttpGet]
         // GET: MemberShipController
         public ActionResult GetMembers()
         {
             return Ok();
         }
 
-        // GET: MemberShipController/Details/5
-        public ActionResult Details(int id)
-        {
-            return Ok();
-        }
+        //// GET: MemberShipController/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return Ok();
+        //}
 
-        // GET: MemberShipController/Create
-        public ActionResult Create()
-        {
-            return Ok();
-        }
+        //// GET: MemberShipController/Create
+        //public ActionResult Create()
+        //{
+        //    return Ok();
+        //}
 
         [Route("AddMember")]
         [HttpPost]
@@ -46,45 +48,45 @@ namespace ChinarDialysisCenter.Server.Controllers
         }
 
         // GET: MemberShipController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return Ok();
-        }
+        //public ActionResult Edit(int id)
+        //{
+        //    return Ok();
+        //}
 
-        // POST: MemberShipController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return Ok();
-            }
-        }
+        //// POST: MemberShipController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return Ok();
+        //    }
+        //}
 
-        // GET: MemberShipController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return Ok();
-        }
+        //// GET: MemberShipController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return Ok();
+        //}
 
-        // POST: MemberShipController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return Ok();
-            }
-        }
+        //// POST: MemberShipController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return Ok();
+        //    }
+        //}
     }
 }
